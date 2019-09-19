c90050a254338de7597507cb15cb51bb7cbecbc2
Michael Kirsch <michaelskirsch@gmail.com>
2019-09-19 12:11:00 -0600

    restructured

--- cat_carousel/index.js
+++ cat_carousel/index.js
@@ -1,7 +1,7 @@
 function handleThumbnailClicks() {
 $('.thumbnail').on('click', e => {
     console.log(e.target);
-    $('.hero img').attr('src',$(e.target).attr('src'));
+    $('.hero img').attr('src',$(e.target).find('img').attr('src'));
     $('.hero img').attr('alt',$(e.target).attr('alt'));
   });
 }
