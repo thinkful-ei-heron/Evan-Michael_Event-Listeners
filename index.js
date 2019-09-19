eac37a67c2dbb2cd020b488c41e394391d5a785b
Evan Vogts <evanjvogts@gmail.com>
2019-09-19 11:41:27 -0500

    Cat Carousel

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+function handleThumbnailClicks() {
 $('.thumbnail').on('click', e => {
     console.log(e.target);
+    $('.hero img').attr('src',$(e.target).attr('src'));
+    $('.hero img').attr('alt',$(e.target).attr('alt'));
   });
-  
+}
+$(handleThumbnailClicks);
